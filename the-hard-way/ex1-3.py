4723569beb646e37aee841adb60aff9145ff7bbc
sharad-vm <sharadvm@gmail.com>
2017-01-25 18:26:12 -0500

    Summarizing Exercises 1-3

---
+++ the-hard-way/ex1-3.py
@@ -0,0 +1,17 @@
+print ("Hello World!")
+print "Hello Again!"
+print("I like typing this.")
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'"
+#print 'I "said" do not touch this.'
+print ("Line #7 has been commented out")
+print 7/4
+print 7.0/4.0
+print 7/4.0
+print 2-1/4+6
+print 3+2+1-5+4
+print 5%8
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3+2 < 5-7
+print "Is it greater?",5 > -2
