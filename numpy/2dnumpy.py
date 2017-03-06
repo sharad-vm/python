7756c8f292740548f225b6d59baf3c926c5f31b1
sharad-vm <sharadvm@gmail.com>
2017-03-06 15:15:08 -0500

    Create 2dnumpy.py

---
+++ numpy/2dnumpy.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# Subway ridership for 5 stations on 10 different days
+ridership = np.array([
+    [   0,    0,    2,    5,    0],
+    [1478, 3877, 3674, 2328, 2539],
+    [1613, 4088, 3991, 6461, 2691],
+    [1560, 3392, 3826, 4787, 2613],
+    [1608, 4802, 3932, 4477, 2705],
+    [1576, 3933, 3909, 4979, 2685],
+    [  95,  229,  255,  496,  201],
+    [   2,    0,    1,   27,    0],
+    [1438, 3785, 3589, 4174, 2215],
+    [1342, 4043, 4009, 4665, 3033]
+])
+
+print ridership[0, :].argmax()
+    
+def mean_riders_for_max_station(np):
+    '''
+    Fill in this function to find the station with the maximum riders on the
+    first day, then return the mean riders per day for that station. Also
+    return the mean ridership overall for comparsion.
+    
+    Hint: NumPy's argmax() function might be useful:
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.argmax.html
+    '''
+    max_riders = np[0, :].argmax()
+    
+    mean_for_max = np[: ,max_riders].mean()
+
+    overall_mean = np.mean()
+    
+    return (overall_mean, mean_for_max)
+
+print mean_riders_for_max_station(ridership)
