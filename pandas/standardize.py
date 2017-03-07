65ad70a6d2256ed5bad3fe88d7cc146b8ecd209d
sharad-vm <sharadvm@gmail.com>
2017-03-07 16:40:11 -0500

    Added use case 2

--- pandas/standardize.py
+++ pandas/standardize.py
@@ -40,3 +40,34 @@ def standardize(df):
     return (df-df.mean())/df.std(ddof=0)
     
 print grades_df.apply(standardize)
+
+'''
+    Apply - Use case 2
+    '''
+
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({
+    'a': [4, 5, 3, 1, 2],
+    'b': [20, 10, 40, 50, 30],
+    'c': [25, 20, 5, 15, 10]
+})
+
+# Change False to True for this block of code to see what it does
+
+# DataFrame apply() - use case 2
+if True:   
+    print df.apply(np.mean)
+    print df.apply(np.max)
+    
+def second_largest(df):
+    '''
+    Fill in this function to return the second-largest value of each 
+    column of the input DataFrame.
+    '''
+    sorted_column = df.sort_values(ascending = False)
+    
+    return sorted_column.iloc[1]
+    
+print df.apply(second_largest)
