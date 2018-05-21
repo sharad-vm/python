fd73a2107738b36b31b805f275db4f303f30cae8
Sharad Vijalapuram <sharadvm@gmail.com>
2018-05-21 18:28:26 -0400

    Adds a function to return last digit of a fibonacci series

--- interviewcake/fibonacci.py
+++ interviewcake/fibonacci.py
@@ -1,10 +1,10 @@
 #Recursive method - slower
 def fib(n):
-    # write the body of your function here
+    # Edge case
     if n < 0:
         return ('Index is negative. '
                 'Index in Fibonacci series cannot be negative.')
-        
+    # Base case    
     elif n in [0, 1]:
         return n
     
@@ -19,11 +19,11 @@ print fib(-1)
 
 #Greedy method
 def fibber(n):
-    # write the body of your function here
+    # Edge case
     if n < 0:
         return ('Index is negative. '
                 'Index in Fibonacci series cannot be negative.')
-        
+    # Base case   
     elif n in [0, 1]:
         return n
     
@@ -40,3 +40,29 @@ def fibber(n):
 print fibber(0)
 print fibber(50)
 print fibber(3)
+
+#Last digit of a large fibonacci number
+
+def lastdigitfib(n):
+    # Edge case
+    if n < 0:
+        return ('Index is negative. '
+                'Index in Fibonacci series cannot be negative.')
+    # Base case    
+    elif n in [0, 1]:
+        return n
+    
+    prev_prev = 0
+    prev = 1
+    
+    for i in xrange(n-1):
+        current = prev_prev + prev
+        prev_prev = prev
+        prev = current
+    
+    return current % 10
+    
+print lastdigitfib(0)
+print lastdigitfib(331)
+print lastdigitfib(3)
+
