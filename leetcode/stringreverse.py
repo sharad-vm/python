dc902d032fdcaab1f18a31910991df11501647be
Sharad Vijalapuram <sharadvm@gmail.com>
2018-09-19 18:25:31 -0400

    return reversed string

---
+++ leetcode/stringreverse.py
@@ -0,0 +1,29 @@
+"""
+Write a function that takes a string as input and returns the string reversed.
+
+Example 1:
+
+Input: "hello"
+Output: "olleh"
+
+Example 2:
+
+Input: "A man, a plan, a canal: Panama"
+Output: "amanaP :lanac a ,nalp a ,nam A"
+"""
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+                
+        i, j = 0, len(s)-1
+        
+        while i < j:
+            
+            s[i], s[j] = s[j], s[i]
+            i+=1
+            j-=1
+            
+        return s
