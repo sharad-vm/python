f136480ffc6b64d1f2166fa20a91d6badbea33c4
Sharad Vijalapuram <sharadvm@gmail.com>
2018-09-27 09:57:03 -0400

    add a solution with XOR

--- leetcode/hammingdistance.py
+++ leetcode/hammingdistance.py
@@ -50,3 +50,26 @@ class Solution(object):
             i-= 1
         
         return binary
+
+"""
+**_Using Bitwise XOR_**
+
+class Solution(object):
+   
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        
+        z = x ^ y
+        count = 0
+        
+        while z > 0:
+            if z % 2 == 1:
+                count += 1
+            z //= 2
+        
+        return count
+  """
