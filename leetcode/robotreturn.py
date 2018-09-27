922a794ee0bdd1ee3bbf977d5aaf536af834d2a6
Sharad Vijalapuram <sharadvm@gmail.com>
2018-09-27 12:56:06 -0400

    add a one-liner, faster solution

--- leetcode/robotreturn.py
+++ leetcode/robotreturn.py
@@ -47,3 +47,16 @@ class Solution(object):
             return True
         
         return False
+
+"""
+One liner - runs in 20ms
+
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        
+        return (moves.count('U') == moves.count('D')) and (moves.count('L') == moves.count('R'))
+"""
