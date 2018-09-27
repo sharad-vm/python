c459508dd7b9b11b679166c23f030d3463d72e43
Sharad Vijalapuram <sharadvm@gmail.com>
2018-09-27 10:11:18 -0400

    return sorted array by parity

---
+++ leetcode/sortarraybyparity.py
@@ -0,0 +1,42 @@
+"""
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+
+Example 1:
+
+Input: [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ 
+
+Note:
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+"""
+class Solution(object):
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        l = 0
+        r = len(A)-1
+        
+        while l<r:
+            
+            while A[l] % 2 == 0 and l<r:
+                l += 1
+            
+            while A[r] % 2 != 0 and l<r:
+                r -= 1
+            
+            temp = A[l]
+            A[l] = A[r]
+            A[r] = temp
+            
+            l += 1
+            r -= 1
+            
+        return A
