31b70c783db5c299eef621f164f3e1e3d3de3247
Sharad Vijalapuram <sharadvm@gmail.com>
2018-09-30 09:34:11 -0400

    add recursive method

--- leetcode/searchBT.py
+++ leetcode/searchBT.py
@@ -46,3 +46,23 @@ class Solution(object):
                 tree = tree.left
             else:
                 return NULL
+
+"""
+Recursive method - faster
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        
+        if not root:
+            return None
+        if root.val==val:
+            return root
+        if root.val < val:
+            return self.searchBST(root.right,val)
+        return self.searchBST(root.left,val)
+"""
