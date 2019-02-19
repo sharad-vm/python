179edbf3be19f174697539c76887607d256d9bba
Sharad Vijalapuram <sharadvm@gmail.com>
2019-02-19 13:18:43 -0800

    find duplicates 2 in an array

---
+++ leetcode/duplicates2.py
@@ -0,0 +1,33 @@
+"""
+Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+"""
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        n = {}
+        
+        for i in range(len(nums)):
+            number = nums[i]
+            if number in n:
+                if abs(i - n[number]) <= k:
+                    return True
+            n[number] = i
+        return False
