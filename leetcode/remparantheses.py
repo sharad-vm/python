d8ea0fb38cb007496dabc31a8a476dbe49e36f8f
Sharad Vijalapuram <sharadvm@gmail.com>
2019-05-26 22:11:41 -0700

    remove the outermost parantheses in a string

---
+++ leetcode/remparantheses.py
@@ -0,0 +1,49 @@
+"""
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+
+"""
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        string = ''
+        open = 0
+        start = 0
+        for i in range(len(S)):
+            if S[i] == "(":
+                open += 1
+            else:
+                open -= 1
+            
+            if open == 0:
+                string += S[start+1:i]
+                start = i+1
+                
+        return string
+                
