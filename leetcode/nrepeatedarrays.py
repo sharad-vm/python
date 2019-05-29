6828f41587345efdf2f8f99574576a030d729bbf
Sharad Vijalapuram <sharadvm@gmail.com>
2019-05-28 21:50:28 -0400

    return the repeated element

---
+++ leetcode/nrepeatedarrays.py
@@ -0,0 +1,32 @@
+"""
+In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+
+Return the element repeated N times.
+
+Example 1:
+
+Input: [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: [5,1,5,2,5,3,5,4]
+Output: 5
+"""
+
+class Solution:
+    def repeatedNTimes(self, A: List[int]) -> int:
+        map = {}
+        
+        for i in A:
+            if i in map:
+                map[i] += 1
+            else:
+                map[i] = 1
+            
+            if map[i] > 1:
+                return i
+         
