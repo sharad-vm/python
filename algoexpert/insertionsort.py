8641932537e3fa430b8dda9482ad3a4b29670d06
Sharad Vijalapuram <sharadvm@gmail.com>
2019-07-20 12:57:09 -0400

    insertion sort

---
+++ algoexpert/insertionsort.py
@@ -0,0 +1,12 @@
+"""
+https://www.algoexpert.io/questions/Insertion%20Sort
+"""
+def insertionSort(array):
+    # Write your code here.
+	for i in range(1, len(array)):
+		j = i
+		while j >0 and array[j]<array[j-1]:
+			array[j], array[j-1] = array[j-1], array[j]
+			j -= 1
+		
+	return array
