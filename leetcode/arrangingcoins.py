3bc0dcb4bf81c5b01e36f1f4381f10c831404274
Sharad Vijalapuram <sharadvm@gmail.com>
2019-08-09 16:05:40 -0400

    returns total no of complete staircases

---
+++ leetcode/arrangingcoins.py
@@ -0,0 +1,38 @@
+"""
+You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+Because the 4th row is incomplete, we return 3.
+"""
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        coin = 1
+        if n == 1:
+            return 1
+        while n >= coin:
+            n = n - coin
+            coin += 1
+        return coin - 1
